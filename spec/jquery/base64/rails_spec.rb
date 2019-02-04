46afc9fbed9ce7afac4c81dc50829ca5ef3f9dec
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 13:05:11 +0900

    Fix spec

--- spec/jquery/base64/rails_spec.rb
+++ spec/jquery/base64/rails_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Jquery::Base64::Rails do
   it "has a version number" do
     expect(Jquery::Base64::Rails::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require "rails/engine"
 require "jquery/base64/rails"
 
 RSpec.configure do |config|
