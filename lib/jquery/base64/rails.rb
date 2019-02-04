b1af06968d885e2dd7bc84203325ce6b983bd220
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 12:55:12 +0900

    initial commit

---
+++ lib/jquery/base64/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/base64/rails/version"
+
+module Jquery
+  module Base64
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/base64/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Base64
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
